6761980b129346137db273e3ed66693ec0037d7d
Cat <catyundo@gmail.com>
2021-06-07 13:04:11 -0700

    phase two specs passing but need to edit path

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -10,37 +10,40 @@ class ControllerBase
   def initialize(req, res)
     @req = req
     @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
-    !!@already_built_response
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "double render not allowed" if already_built_response?
     res.status = 302
     res["Location"] = url
-    temp = @already_built_response
     @already_built_response = true
-    raise "double render not allowed" if temp == @already_built_response
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type = "text/html")
+    raise "double render not allowed" if already_built_response?
     res.write(content)
     res['Content-Type'] = content_type
-    temp = @already_built_response
     @already_built_response = content
-    raise "double render not allowed" if temp == content
-
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    raise "double render not allowed" if already_built_response?
+    # File.dirname(__FILE__) # views/cats_controller => we want to pass index
+    content = File.read("views/cats_controller/index.html.erb")
+    render_content(ERB.new(content).result(binding), content_type = "text/html")
+    @already_built_response = true
   end
 
   # method exposing a `Session` object
@@ -52,3 +55,6 @@ class ControllerBase
   end
 end
 
+
+
+# path == "/cats"
